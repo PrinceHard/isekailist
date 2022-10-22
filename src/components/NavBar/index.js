2626bf8ef0653f81adb461d7129e0045125c1c83
Héric Virgilio <102180379+HericVirgilio@users.noreply.github.com>
2022-10-22 17:56:17 -0300

    commit banner, alteração na imagem

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,26 +4,28 @@ import "./style.css"
 
 function NavBar() {
     return (
-        <nav>
-            <div className='logo'>
-                <img src={Logo}></img>
-                <h1>SekaiList</h1>
-            </div>
-            <div>
-                <SearchBar />
-            </div>
-            <div className='menu'>
-                <ul>
-                    <li><a href="#">Animes</a></li>
-                </ul>
-                <ul>
-                    <li><a href="#">Mangás</a></li>
-                </ul>
-                <ul>
-                    <li><a href="#">Profile</a></li>
-                </ul>
-            </div>
-        </nav>
+        <div className='Banner'>
+            <nav>
+                <div className='logo'>
+                    <img src={Logo}></img>
+                    <h1>SekaiList</h1>
+                </div>
+                <div>
+                    <SearchBar />
+                </div>
+                <div className='menu'>
+                    <ul>
+                        <li><a href="#">Animes</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Mangás</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Profile</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
 export default NavBar
