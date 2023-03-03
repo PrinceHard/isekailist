ab201729154e35b2962f43c72c0c442fd5be6bdb
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-03-03 16:00:00 -0300

    mock the card section list image

---
+++ src/data/section.js
@@ -0,0 +1,11 @@
+export const list = [
+    {id: 1, url: "http://localhost:3000/kimetsu.jpg"},
+    {id: 2, url: "http://localhost:3000/kimetsu.jpg"},
+    {id: 3, url: "http://localhost:3000/kimetsu.jpg"},
+    {id: 4, url: "http://localhost:3000/kimetsu.jpg"},
+    {id: 5, url: "http://localhost:3000/kimetsu.jpg"},
+    {id: 6, url: "http://localhost:3000/kimetsu.jpg"},
+    {id: 7, url: "http://localhost:3000/kimetsu.jpg"},
+    {id: 8, url: "http://localhost:3000/kimetsu.jpg"},
+    {id: 9, url: "http://localhost:3000/kimetsu.jpg"},
+]
