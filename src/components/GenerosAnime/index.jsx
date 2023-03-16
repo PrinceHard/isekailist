e7bf65e3d50114ae3e33823bd2f34fd0b3c45105
Héric Virgilio <102180379+HericVirgilio@users.noreply.github.com>
2023-03-16 00:04:40 -0300

    gender div change

--- src/components/GenerosAnime/index.jsx
+++ src/components/GenerosAnime/index.jsx
@@ -4,13 +4,14 @@ import { Link } from "react-router-dom"
 const GenerosAnime = () => {
     return (
         <div>
-            <h1>Gêneros Anime</h1>
+            <h1 className="title-level1-genre">Gêneros Anime</h1>
             <div className="genero-container">
                 {lista.map((genero) => (
-                    <Link to={`/animes/generos/${genero.id}`}>
-                        <div key={genero.id} style={{ backgroundImage: `url(${genero.img})`, filter: 'brightness(75%)' }}>
-                            {genero.name}
-                        </div>
+                    <Link to={`/animes/generos/${genero.id}`}>  
+                            <div style={{width:'290px',height: '290px'}}>
+                                <h3 className="title-generos">{genero.name}</h3>
+                                <div key={genero.id} style={{ backgroundImage: `url(${genero.img})`, filter: 'brightness(70%)' }} className="card-genero"></div>
+                            </div>
                     </Link>
                 ))}
             </div>
