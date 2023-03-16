be40c3098cdb7c65b646cce5dca89cce7b36852a
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-03-16 00:11:04 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,17 @@ import './App.css';
 import AnimeSection from './components/AnimeSection';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header';
-import MangasPage from './pages/MangasPage';
 const App = () => {
   return (
     <div>
-      {/* <div className='background'>
+      <div className='background'>
         <Header />
         <AnimeSection id={"section-1"} nameSection={"Animes Season"}/>
         <AnimeSection  id={"section-2"} nameSection={"Most Popular Animes"}/>
         <AnimeSection id={"section-3"} nameSection={"Most Read Mangas"}/>
         <AnimeSection id={"section-4"} nameSection={"News"}/>
         <Footer />
-      </div> */}
-      <MangasPage/>
+      </div>
     </div>
   );
 }
