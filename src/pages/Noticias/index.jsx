f995bc685878130a5be867d3c9398d1c58eb29c0
Héric Virgilio <102180379+HericVirgilio@users.noreply.github.com>
2023-03-21 16:28:30 -0300

    finish news page

--- src/pages/Noticias/index.jsx
+++ src/pages/Noticias/index.jsx
@@ -2,7 +2,7 @@ import NavBar from "../../components/NavBar"
 import Footer from "../../components/Footer/Footer"
 import './style.css'
 import { noticias } from "../../data/news"
-import  Scans  from "../../components/Scans"
+import Scans from "../../components/Scans"
 const Noticias = () => {
     return (
         <div className="background">
@@ -11,7 +11,7 @@ const Noticias = () => {
                 <div className="bannerNoticias"></div>
                 <h1>Notícias</h1>
                 <div className="containerScanNews">
-                    <Scans/>
+                    <Scans />
                     <div className="noticiasBox">
                         {noticias.map((noticia) => (
                             <div><a href={noticia.link} target="_blank"><img src={noticia.noticia} alt="" /></a></div>
