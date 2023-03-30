7693f96ee8b12ab54443861b10d237b5aac14cd8
Héric Virgilio <102180379+HericVirgilio@users.noreply.github.com>
2023-03-30 19:55:34 -0300

    implementation autocomplete

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -2,7 +2,8 @@ import './style.css'
 import { ReactComponent as Search } from '../../assets/svg/search.svg'
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import useFetch from '../../hooks/useFetch';
+import Erro from '../../pages/Erro';
 const SearchBar = () => {
 
     const [searchValor, setSearch] = useState('');
@@ -11,14 +12,24 @@ const SearchBar = () => {
         setSearch(evento.target.value)
     }
 
+    const { data, error, isLoading } = useFetch(`https://api.jikan.moe/v4/anime`);
+
+    if (isLoading) {
+        return <p>Carregando...</p>
+    }
+    if (error) {
+        return <Erro />
+    }
+
     return (
         <div className='container'>
-            <form onSubmit="">
+            <form onSubmit="" className='pesquisa'>
                 <input
                     placeholder='Pesquise um anime'
                     className='searchBar'
                     value={searchValor}
                     onChange={handleChange}
+                    list="sugestao"
                 />
                 <Link to={"/busca/" + searchValor} >
                     <button type='submit' className='buttonSearch'>
@@ -26,6 +37,9 @@ const SearchBar = () => {
                     </button>
                 </Link>
             </form>
+            <datalist id='sugestao' className='barraDeAutoComplete'>
+                {data.data.map((complite) => (<option value={complite.title}></option>))}
+            </datalist>
         </div>
     )
 }
