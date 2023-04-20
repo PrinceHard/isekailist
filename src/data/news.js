2a4d77814d94d0134414cacd97c7d55d3375d4cd
Héric Virgilio <102180379+HericVirgilio@users.noreply.github.com>
2023-04-20 15:35:56 -0300

    images atualization from vercel

--- src/data/news.js
+++ src/data/news.js
@@ -1,79 +1,79 @@
 export const news =[
     {
-        noticia: "http://localhost:3000/news1.PNG",
+        noticia: "https://sekailist.vercel.app//news1.PNG",
         link: "https://www.intoxianime.com/2023/03/dead-mount-death-isekai-do-autor-de-durarara-ganha-trailer-com-op-e-data-de-estreia/"
     },
     {
-        noticia: "http://localhost:3000/news2.PNG",
+        noticia: "https://sekailist.vercel.app//news2.PNG",
         link: "https://www.intoxianime.com/2023/03/dr-stone-3o-temporada-ganha-trailer-completo-com-op-e-data-de-estreia/"
     },
     {
-        noticia: "http://localhost:3000/news3.PNG",
+        noticia: "https://sekailist.vercel.app//news3.PNG",
         link: "https://www.intoxianime.com/2023/03/boushoku-no-berserk-anime-sobre-protagonista-devorador-de-almas-ganha-teaser-e-janela-de-estreia/"
     },
     {
-        noticia: "http://localhost:3000/news4.PNG",
+        noticia: "https://sekailist.vercel.app//news4.PNG",
         link: "https://www.intoxianime.com/2023/03/yamada-kun-to-lv999-comedia-romantica-entre-jogadores-de-mmo-ganha-trailer-com-op/"
     }
 ]
 export const noticias =[
     {
-        noticia: "http://localhost:3000/news0.PNG",
+        noticia: "https://sekailist.vercel.app//news0.PNG",
         link: "https://www.intoxianime.com/2023/03/guia-de-novos-animes-de-abril-2023/",
         id: 1
     },
     {
-        noticia: "http://localhost:3000/news1.PNG",
+        noticia: "https://sekailist.vercel.app//news1.PNG",
         link: "https://www.intoxianime.com/2023/03/dead-mount-death-isekai-do-autor-de-durarara-ganha-trailer-com-op-e-data-de-estreia/",
         id: 2
     },
     {
-        noticia: "http://localhost:3000/news2.PNG",
+        noticia: "https://sekailist.vercel.app//news2.PNG",
         link: "https://www.intoxianime.com/2023/03/dr-stone-3o-temporada-ganha-trailer-completo-com-op-e-data-de-estreia/",
         id: 3
     },
     {
-        noticia: "http://localhost:3000/news3.PNG",
+        noticia: "https://sekailist.vercel.app//news3.PNG",
         link: "https://www.intoxianime.com/2023/03/boushoku-no-berserk-anime-sobre-protagonista-devorador-de-almas-ganha-teaser-e-janela-de-estreia/",
         id: 4
     },
     {
-        noticia: "http://localhost:3000/news4.PNG",
+        noticia: "https://sekailist.vercel.app//news4.PNG",
         link: "https://www.intoxianime.com/2023/03/yamada-kun-to-lv999-comedia-romantica-entre-jogadores-de-mmo-ganha-trailer-com-op/",
         id: 5
     },
     {
-        noticia: "http://localhost:3000/news5.PNG",
+        noticia: "https://sekailist.vercel.app//news5.PNG",
         link: "https://www.intoxianime.com/2023/03/tonikaku-kawaii-2o-temporada-ganha-trailer-com-op-e-data-de-estreia/",
         id: 6
     },
     {
-        noticia: "http://localhost:3000/news6.PNG",
+        noticia: "https://sekailist.vercel.app//news6.PNG",
         link: "https://www.intoxianime.com/2023/02/rumor-kage-no-jitsuryokusha-2o-temporada-deve-estrear-em-outubro/",
         id: 7
     },
     {
-        noticia: "http://localhost:3000/news7.PNG",
+        noticia: "https://sekailist.vercel.app//news7.PNG",
         link: "https://www.intoxianime.com/2023/03/guia-de-novos-animes-de-abril-2023/",
         id: 8
     },
     {
-        noticia: "http://localhost:3000/news8.PNG",
+        noticia: "https://sekailist.vercel.app//news8.PNG",
         link: "https://www.intoxianime.com/2023/03/dead-mount-death-isekai-do-autor-de-durarara-ganha-trailer-com-op-e-data-de-estreia/",
         id: 9
     },
     {
-        noticia: "http://localhost:3000/news9.PNG",
+        noticia: "https://sekailist.vercel.app//news9.PNG",
         link: "https://www.intoxianime.com/2023/03/dr-stone-3o-temporada-ganha-trailer-completo-com-op-e-data-de-estreia/",
         id: 10
     },
     {
-        noticia: "http://localhost:3000/news3.PNG",
+        noticia: "https://sekailist.vercel.app//news3.PNG",
         link: "https://www.intoxianime.com/2023/03/boushoku-no-berserk-anime-sobre-protagonista-devorador-de-almas-ganha-teaser-e-janela-de-estreia/",
         id: 11
     },
     {
-        noticia: "http://localhost:3000/news4.PNG",
+        noticia: "https://sekailist.vercel.app//news4.PNG",
         link: "https://www.intoxianime.com/2023/03/yamada-kun-to-lv999-comedia-romantica-entre-jogadores-de-mmo-ganha-trailer-com-op/",
         id: 12
     },
