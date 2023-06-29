7db195425069d6912cd4b798776ea3560c4186f1
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-06-29 16:03:45 -0300

    fix: removed list on home page

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
         <div className='slyderBox'>
           <AnimesSeason/>
           <MostPopularAnimes/>
-          <MostPopularMangas/>
         </div>
         <Footer />
       </div>
